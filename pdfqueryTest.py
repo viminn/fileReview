dbde2107ad15277dc47718c846f34a0c9dd01a89
viminn <123514087+viminn@users.noreply.github.com>
2024-10-14 23:58:57 -0400

    Add new output

--- pdfqueryTest.py
+++ pdfqueryTest.py
@@ -2,13 +2,14 @@ import re
 import pdfplumber
 
 file = "UnofficialTranscript.pdf"
-output_text_file_path = 'output.txt'
+fullOutput = 'output.txt'
+justHonors = "honors.txt"
 roughRE = "^([a-zA-Z]{3,4})\s*(\d{1,3})\s*(.*?)(HONORS.*|\n.*?HONORS.*)"
 # not working step2 = "^[A-Z]{3} \d{3} Kutztown UG .+ HONORS [A-F][+-]? \d\.\d{3} \d{2}\.\d{2}$"
 
 # Open the PDF file and extract text
 with pdfplumber.open(file) as pdf:
-    with open(output_text_file_path, 'w', encoding='utf-8') as text_file:
+    with open(fullOutput, 'w', encoding='utf-8') as text_file:
         for page in pdf.pages:
             text = page.extract_text()
             if text:  # Check if text was extracted
